7d4da823b197287a38fd284fc3b52748dc12b053
mantajdhillon <mantajdhillon91@gmail.com>
2024-03-09 22:18:44 -0800

    Added cohere code to index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
-let boolDiscover = 1;
-let boolExplore = 0;
-let boolRecipes = 0;
+import * as cohere from 'cohere';
+
+const { CohereClient } = require("cohere-ai");
+
+const cohere = new CohereClient({
+    token: "l1oflAR4IgAwFhH4vzr8WO41F2OcbPsrWUv5zWd2",
+});
+
+
+function getResponse() {
+
+    var promptString = document.getElementById("string");
+
+    (async () => {
+        const generate = await cohere.generate({
+            // prompt: "Please explain to me how LLMs work",
+            prompt: promptString,
+        });
+      
+        console.log(generate);
+      })();
+
+
+
+}
+
+
+// co = new cohere.Client("0qvGBtiVZattxDeOgW9WQPgKjACubaQNp4GblUVK");
+// headers = {
+//   "x-api-key": "l1oflAR4IgAwFhH4vzr8WO41F2OcbPsrWUv5zWd2"
+// };
